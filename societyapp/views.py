1eb0bc392099929a3d9a19ae3f199eb345abb342
John_V_Coder <omondijohn669@gmail.com>
2025-06-16 00:12:28 +0300

    any unsaved change

--- societyapp/views.py
+++ societyapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from societyapp.models import Contact, BlogPost, Donation, green_campaign, Event, FarmingCourse, stories
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
